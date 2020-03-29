fef2aa251f540aa5d4fdd25eec7287dbecb68503
FirnasData <62829876+FirnasData@users.noreply.github.com>
2020-03-29 13:58:24 +0600

    html, css, bootstrap, javascript, ajax

---
+++ js/main.js
@@ -0,0 +1,227 @@
+function c2tabspn()
+{   var cup=document.getElementById("cupid").value;
+    var tspn=cup*16;
+    document.getElementById("tspnid").innerHTML=tspn;
+}
+function c2tespn()
+{   var cup=document.getElementById("cupid").value;
+    var tespn=cup*48;
+    document.getElementById("tsepnid").innerHTML=tespn;
+}
+function c2grm()
+{   var cup=document.getElementById("cupid").value;
+    var grm=cup*227;
+    document.getElementById("grmid").innerHTML=grm;
+}
+function c2ons()
+{   var cup=document.getElementById("cupid").value;
+    var ons=cup*8;
+    document.getElementById("onsid").innerHTML=ons;
+}
+function gln2ml()
+{   var gln=document.getElementById("glnid").value;
+    var ml=gln*3785.41;
+    document.getElementById("mlid").innerHTML=ml;
+}
+function gln2ons()
+{   var gln=document.getElementById("glnid").value;
+    var ons=gln*128;
+    document.getElementById("onsid").innerHTML=ons;
+}
+function gln2ons()
+{   var gln=document.getElementById("glnid").value;
+    var ons=gln*128;
+    document.getElementById("onsid").innerHTML=ons;
+}
+function grm2pnd()
+{   var grm=document.getElementById("grmid").value;
+    var pnd=grm*8.35;
+    document.getElementById("pndid").innerHTML=pnd;
+}
+function grm2kg()
+{   var grm=document.getElementById("grmid").value;
+    var kg=grm*0.001;
+    document.getElementById("kgid").innerHTML=kg;
+}
+function grm2cp()
+{
+    var grm=document.getElementById("grmid").value;
+    var cp=grm*.125;
+    document.getElementById("pndid").innerHTML=pnd;
+}
+
+function grm2onc()
+{
+    var grm=document.getElementById("grmid").value;
+    var onc=grm*28.35;
+    document.getElementById("oncid").innerHTML=onc;
+}
+
+function kg2lbs()
+{
+    var kg=document.getElementById("kgid").value;
+    var lbs=kg*2.20462;
+    document.getElementById("lbsid").innerHTML=lbs;
+}
+function kg2pnd()
+{
+    var kg=document.getElementById("kgid").value;
+    var pnd=kg*2.20462;
+    document.getElementById("pndid").innerHTML=pnd;
+}
+
+function kg2grm()
+{
+    var kg=document.getElementById("kgid").value;
+    var grm=kg*1000;
+    document.getElementById("grmid").innerHTML=grm;
+}
+function lbs2kg()
+{
+    var lbs=document.getElementById("lbsid").value;
+    var kg=lbs*0.453592;
+    document.getElementById("kgid").innerHTML=kg;
+}
+//editing purpose start from here.
+//editing purpose start from here..
+//editing purpose start from here...
+function lbs2kg()
+{
+    var lbs=document.getElementById("lbsid").value;
+    var kg=lbs*8.35;
+    document.getElementById("kgid").innerHTML=kg;
+}
+ 
+function lbs2stone()
+{
+    var lbs=document.getElementById("lbsid").value;
+    var stone=lbs*8.35;
+    document.getElementById("stoneid").innerHTML=stone;
+}
+
+function ml2gln()
+{
+    var ml=document.getElementById("mlid").value;
+    var gln=ml*8.35;
+    document.getElementById("glnid").innerHTML=gln;
+}
+function ml2tbspn()
+{
+    var ml=document.getElementById("mlid").value;
+    var tbspn=ml*8.35;
+    document.getElementById("tbspnid").innerHTML=tbspn;
+}
+function ons2ml()
+{
+    var ons=document.getElementById("onsid").value;
+    var ml=ons*8.35;
+    document.getElementById("mlid").innerHTML=ml;
+}
+function ons2tbspn()
+{
+    var ons=document.getElementById("onsid").value;
+    var tbspn=ons*8.35;
+    document.getElementById("tbspnid").innerHTML=tbspn;
+}
+function ons2tespn()
+{
+    var ons=document.getElementById("onsid").value;
+    var tespn=ons*8.35;
+    document.getElementById("tespnid").innerHTML=tespn;
+}
+function ons2cup()
+{
+    var ons=document.getElementById("onsid").value;
+    var cup=ons*8.35;
+    document.getElementById("cupid").innerHTML=cup;
+}
+function ons2gln()
+{
+    var ons=document.getElementById("onsid").value;
+    var gln=ons*8.35;
+    document.getElementById("glnid").innerHTML=gln;
+}
+function ons2grm()
+{
+    var ons=document.getElementById("onsid").value;
+    var grm=ons*8.35;
+    document.getElementById("grmid").innerHTML=grm;
+}
+function pnd2grm()
+{
+    var pnd=document.getElementById("pndid").value;
+    var grm=pnd*8.35;
+    document.getElementById("grmid").innerHTML=grm;
+}
+function pnd2kn()
+{
+    var pnd=document.getElementById("pndid").value;
+    var kg=pnd*8.35;
+    document.getElementById("kgid").innerHTML=kg;
+}
+function qrt2tbspn()
+{
+    var qrt=document.getElementById("qrtid").value;
+    var tbspn=ons*8.35;
+    document.getElementById("tbspnid").innerHTML=tbspn;
+}
+function qrt2tespn()
+{
+    var qrt=document.getElementById("qrtid").value;
+    var tespn=ons*8.35;
+    document.getElementById("tespnid").innerHTML=tespn;
+}
+function stn2lbs()
+{
+    var stn=document.getElementById("stnid").value;
+    var lbs=stn*8.35;
+    document.getElementById("lbsid").innerHTML=lbs;
+}
+function tblspn2cup()
+{
+    var tblspn=document.getElementById("tblspnid").value;
+    var cup=tblspn*8.35;
+    document.getElementById("cupid").innerHTML=cup;
+}
+function tblspn2ml()
+{
+    var tblspn=document.getElementById("tblspnid").value;
+    var ml=tblspn*8.35;
+    document.getElementById("mlid").innerHTML=ml;
+}
+function tblspn2onc()
+{
+    var tblspn=document.getElementById("tblspnid").value;
+    var onc=tblspn*8.35;
+    document.getElementById("oncid").innerHTML=onc;
+}
+function tblspn2tespn()
+{
+    var tblspn=document.getElementById("tblspnid").value;
+    var tespn=tblspn*8.35;
+    document.getElementById("tespnid").innerHTML=tespn;
+}
+function tespn2cup()
+{
+    var tespn=document.getElementById("tespnid").value;
+    var cup=tespn*8.35;
+    document.getElementById("cupid").innerHTML=cup;
+}
+function tespn2ml()
+{
+    var tespn=document.getElementById("tespnid").value;
+    var ml=tespn*8.35;
+    document.getElementById("mlid").innerHTML=ml;
+}
+function tespn2onc()
+{
+    var tespn=document.getElementById("tespnid").value;
+    var onc=tespn*8.35;
+    document.getElementById("oncid").innerHTML=onc;
+}
+function tespn2tblspn()
+{
+    var tespn=document.getElementById("tespnid").value;
+    var tblspn=tespn*8.35;
+    document.getElementById("tblspnid").innerHTML=tblspn;
+}
